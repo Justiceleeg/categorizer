ad934f5595c4e8da550fed05bd9e772fd95d00b7
Justice White <jaxingermany@Justice-Whites-MacBook-Pro.local>
2017-06-14 21:45:33 -0600

    steps up to but not 12

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
+// 
+// STOPPED AT/Before STEP 12
+// 
+
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 import "./App.css";
 
 import NewChart from "./NewChart/NewChart";
 import Sidebar from "./Sidebar/Sidebar";
 import AddDataset from "./AddDataset/AddDataset";
+import ActiveChart from './ActiveChart/ActiveChart';
+
+import { createChart } from '../ducks/chart';
 
 class App extends Component {
 	render() {
+		const {
+			activeChart,
+			charts,
+			createChart
+		} = this.props;
 		return (
 			<div className="app">
 				<Sidebar />
@@ -20,7 +33,7 @@ class App extends Component {
 						</div>
 					</header>
           <div className="app__active-chart">
-            
+            <ActiveChart chart={ activeChart }/>
           </div>
 				</main>
 			</div>
@@ -28,4 +41,12 @@ class App extends Component {
 	}
 }
 
-export default App;
+function mapStateToProps( {activeChartIndex, charts}){
+	return {
+		activeChart: charts[ activeChartIndex ], 
+		charts
+	}
+}
+
+export default connect(mapStateToProps, { createChart })(App);
+
